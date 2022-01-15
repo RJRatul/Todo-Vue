d7d3b0f0ca9e33f8b4499b220960d2c1bd3734c1
RJRatul <rafsan.ratul.mians@gmail.com>
2022-01-15 23:37:35 +0600

    Todo List Functionality Completed

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app" class="container">
+
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+  }
+}
+</script>
+
+<style>
+
+*{
+  box-sizing: border-box;
+}
+
+.container{
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 24px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,229 @@
+<template>
+  <div>
+    <input
+      type="text"
+      class="todo-input"
+      placeholder="What needs to be done"
+      v-model="newTodo"
+      @keyup.enter="addTodo"
+    />
+    <transition-group
+      name="fade"
+      enter-active-class="animated fadeInUp"
+      leave-active-class="animated fadeOutDown"
+    >
+      <todo-item
+        v-for="todo in todosFiltered"
+        :key="todo.id"
+        :todo="todo"
+        :checkAll="!anyRemaining"
+        @removedTodo="removeTodo"
+        @finishedEdit="finishedEdit"
+      >
+      </todo-item>
+    </transition-group>
+
+    <div class="extra-container">
+      <div>
+        <label
+          ><input
+            type="checkbox"
+            :checked="!anyRemaining"
+            @change="checkAllTodos"
+          />
+          Check All</label
+        >
+      </div>
+      <div>{{ remaining }} items left</div>
+    </div>
+
+    <div class="extra-container">
+      <div>
+        <button :class="{ active: filter == 'all' }" @click="filter = 'all'">
+          All
+        </button>
+        <button
+          :class="{ active: filter == 'active' }"
+          @click="filter = 'active'"
+        >
+          Active
+        </button>
+        <button
+          :class="{ active: filter == 'completed' }"
+          @click="filter = 'completed'"
+        >
+          Completed
+        </button>
+      </div>
+
+      <div>
+        <transition name="fade">
+          <button v-if="showClearCompletedButton" @click="clearCompleted">
+            Clear Completed
+          </button>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem";
+export default {
+  name: "todo-list",
+  components: {
+    TodoItem,
+  },
+  data() {
+    return {
+      newTodo: "",
+      idForTodo: 3,
+      filter: "all",
+      todos: [
+        {
+              id: 1,
+              title: "Add a new todo",
+              completed: false,
+              editing: false,
+            },
+            {
+              id: 2,
+              title: "Edit a todo by double clicking on it",
+              completed: false,
+              editing: false,
+            },
+            {
+              id: 3,
+              title: "Remove a todo by clicking the X icon",
+              completed: false,
+              editing: false,
+            },
+            {
+              id: 4,
+              title: "Remove all todos by selecting all",
+              completed: false,
+              editing: false,
+            },
+      ],
+    };
+  },
+  computed: {
+    remaining() {
+      return this.todos.filter((todo) => !todo.completed).length;
+    },
+    anyRemaining() {
+      return this.remaining != 0;
+    },
+    todosFiltered() {
+      if (this.filter == "all") {
+        return this.todos;
+      } else if (this.filter == "active") {
+        return this.todos.filter((todo) => !todo.completed);
+      } else if (this.filter == "completed") {
+        return this.todos.filter((todo) => todo.completed);
+      }
+      return this.todos;
+    },
+    showClearCompletedButton() {
+      return this.todos.filter((todo) => todo.completed).length > 0;
+    },
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim().length == 0) {
+        return;
+      }
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false,
+      });
+      this.newTodo = "";
+      this.idForTodo++;
+    },
+    removeTodo(id) {
+      const index = this.todos.findIndex((item) => item.id == id);
+      this.todos.splice(index, 1);
+    },
+    checkAllTodos() {
+      this.todos.forEach((todo) => (todo.completed = event.target.checked));
+    },
+    clearCompleted() {
+      this.todos = this.todos.filter((todo) => !todo.completed);
+    },
+    finishedEdit(data) {
+      const index = this.todos.findIndex((item) => item.id == data.id);
+      this.todos.splice(index, 1, data);
+    },
+  },
+};
+</script>
+
+<style>
+@import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
+.todo-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
+}
+.todo-item {
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  animation-duration: 0.3s;
+}
+.remove-item {
+  cursor: pointer;
+  margin-left: 14px;
+}
+.todo-item-left {
+  display: flex;
+  align-items: center;
+}
+.todo-item-label {
+  padding: 10px;
+  border: 1px solid white;
+  margin-left: 12px;
+}
+.todo-item-edit {
+  font-size: 24px;
+  color: #2c3e50;
+  margin-left: 12px;
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
+.completed {
+  text-decoration: line-through;
+  color: grey;
+}
+.extra-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  border-top: 1px solid lightgrey;
+  padding-top: 14px;
+  margin-bottom: 14px;
+}
+button {
+  font-size: 14px;
+  background-color: white;
+  appearance: none;
+}
+.active {
+  background: lightgreen;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
